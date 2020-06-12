6fa94f4ce00dd5b5382b32a86fcb5fce1e090f9f
Yohannes Libanos <yhlibanos@gmail.com>
2020-06-12 01:33:25 +0100

    Add turnstile model

--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -14,12 +14,9 @@ logger = logging.getLogger(__name__)
 class Turnstile(Producer):
     key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/turnstile_key.json")
 
-    #
-    # TODO: Define this value schema in `schemas/turnstile_value.json, then uncomment the below
-    #
-    #value_schema = avro.load(
-    #    f"{Path(__file__).parents[0]}/schemas/turnstile_value.json"
-    #)
+    value_schema = avro.load(
+       f"{Path(__file__).parents[0]}/schemas/turnstile_value.json"
+    )
 
     def __init__(self, station):
         """Create the Turnstile"""
@@ -31,18 +28,12 @@ class Turnstile(Producer):
             .replace("'", "")
         )
 
-        #
-        #
-        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
-        # replicas
-        #
-        #
         super().__init__(
-            f"{station_name}", # TODO: Come up with a better topic name
+            f"cta.stations.{station_name}.turnstile", 
             key_schema=Turnstile.key_schema,
-            # TODO: value_schema=Turnstile.value_schema, TODO: Uncomment once schema is defined
-            # TODO: num_partitions=???,
-            # TODO: num_replicas=???,
+            value_schema=Turnstile.value_schema, 
+            num_partitions=5,
+            num_replicas=3,
         )
         self.station = station
         self.turnstile_hardware = TurnstileHardware(station)
@@ -51,9 +42,12 @@ class Turnstile(Producer):
         """Simulates riders entering through the turnstile."""
         num_entries = self.turnstile_hardware.get_entries(timestamp, time_step)
         logger.info("turnstile kafka integration incomplete - skipping")
-        #
-        #
-        # TODO: Complete this function by emitting a message to the turnstile topic for the number
-        # of entries that were calculated
-        #
-        #
+        self.producer.produce(
+           topic=self.topic_name,
+           key={"timestamp": timestamp},
+           value={
+               "station_id": self.station.station_id,
+               "station_name": self.station.station_name,
+               "num_entries": num_entries
+           },
+        )
